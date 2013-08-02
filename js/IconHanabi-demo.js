bfcb5b43639e61cac1f659aad82cddca0e1f5e5c
Fujisawa Shin <fujisawa-shin@kayac.com>
2013-08-02 16:16:07 +0900

    custumize demo. (add type b).

--- js/IconHanabi-demo.js
+++ js/IconHanabi-demo.js
@@ -2,49 +2,101 @@ $(function () {
     var img = new Image();
     img.src = 'img/sample.png';
 
+    var opts = {
+        icon: img,
+        // iconSize: 80,
+
+        // size: 300,
+
+        // lineColor: 'rgba(255, 255, 0, 0.5),'
+
+        id: 'icon-hanabi-' + (new Date().getTime()),
+        className: 'icon-hanabi',
+
+        // clock: 20,
+
+        tailTime: 200,
+        attackTime: 200,
+        releaseTime: 300
+    };
+
     $(document.body).on('click', function (e) {
-        new IconHanabi({
-            icon: img,
-            // iconSize: 80,
+        opts.x = e.pageX;
+        opts.y = e.pageY;
+        new IconHanabi(opts);
+    });
 
-            x: e.pageX,
-            y: e.pageY,
-            // size: 300,
-            minSize: 200,
-            maxSize: 400,
+    $('#button-a').on('click', function (e) {
+        e.stopPropagation();
 
-            // lineColor: 'rgba(255, 255, 0, 0.5),'
+        opts.minSize = 200;
+        opts.maxSize = 400;
 
-            id: 'icon-hanabi-' + (new Date().getTime()),
-            className: 'icon-hanabi',
+        opts.rings = [{
+            offsetTime: 0,
+            count: 1,
+            scale: 0,
+            iconScale: 0.8
+        }, {
+            offsetTime: 50,
+            count: 28,
+            scale: 0.3,
+            iconScale: 0.8
+        }, {
+            offsetTime: 50,
+            count: 28,
+            scale: 0.6,
+            iconScale: 0.8
+        }, {
+            offsetTime: 100,
+            count: 28,
+            scale: 1,
+            iconScale: 1
+        }];
+    }).click();
 
-            // clock: 20,
 
-            tailTime: 200,
-            attackTime: 200,
-            releaseTime: 300,
+    $('#button-b').on('click', function (e) {
+        e.stopPropagation();
 
-            rings: [{
-                offsetTime: 0,
-                count: 1,
-                scale: 0,
-                iconScale: 0.8
-            }, {
-                offsetTime: 50,
-                count: 28,
-                scale: 0.3,
-                iconScale: 0.8
-            }, {
-                offsetTime: 50,
-                count: 28,
-                scale: 0.6,
-                iconScale: 0.8
-            }, {
-                offsetTime: 100,
-                count: 28,
-                scale: 1,
-                iconScale: 1
-            }]
-        });
+        opts.minSize = 300;
+        opts.maxSize = 500;
+
+        opts.rings = [{
+            offsetTime: 0,
+            count: 1,
+            scale: 0,
+            iconScale: 1
+        }, {
+            offsetTime: 0,
+            count: 8,
+            scale: 0.15,
+            iconScale: 0.8
+        }, {
+            offsetTime: 0,
+            count: 8,
+            scale: 0.3,
+            iconScale: 0.8
+        }, {
+            offsetTime: 0,
+            count: 8,
+            scale: 0.45,
+            iconScale: 0.8
+        }, {
+            offsetTime: 0,
+            count: 8,
+            scale: 0.6,
+            iconScale: 0.8
+        }, {
+            offsetTime: 0,
+            count: 8,
+            scale: 0.75,
+            iconScale: 0.8
+        }, {
+            offsetTime: 0,
+            count: 8,
+            scale: 0.9,
+            iconScale: 1
+        }];
     });
 });
